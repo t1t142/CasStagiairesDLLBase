64bc2968dee1d5afd3ab4e5b15a6a2ddda2aa315
thierry verrier <thierryverrier@orange.fr>
2018-05-29 13:19:39 +0200

    ajout branche gestionSection et form liste Section

---
+++ CasStagiaire/frmListeSection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CasStagiaire
+{
+    public partial class frmListeSection : Form
+    {
+        public frmListeSection()
+        {
+            InitializeComponent();
+        }
+    }
+}
